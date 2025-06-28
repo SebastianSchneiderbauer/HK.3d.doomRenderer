f3d6037f9d3a80d5ad124c2a238f0f864073a2de
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-06-28 18:10:46 +0200

    setup project

---
+++ project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HK._3d.doomRenderer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
