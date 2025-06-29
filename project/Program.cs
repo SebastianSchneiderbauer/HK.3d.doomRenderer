e07f751861e23fb135910bc50ebe46ec76754332
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-06-29 16:11:18 +0200

    set up basics

--- project/Program.cs
+++ project/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // id´s of maptiles:
+            // 0 = empty
+            // 1 = wall
+            int[,] map = new int[11, 11];
+
+            double playerX = 6;
+            double playerY = 6;
+
+            int fov = 60; // in degrees
+            double viewdistance = 10.0; // in mapunits(one index in the map array is one mapunit)
+
+            // create test map
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                if (i == 0 || i == map.GetLength(0) - 1)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        map[i, j] = 1;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        if (j == 0 || j == map.GetLength(1) - 1)
+                        {
+                            map[i, j] = 1;
+                        }
+                    }
+                }
+            }
+
+            // visualize the testmap
+            printMap(map);
+        }
+
+        static void printMap(int[,] map)
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    Console.Write(map[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
