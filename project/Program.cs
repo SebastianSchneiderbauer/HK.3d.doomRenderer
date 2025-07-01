c791ca651764f984da2d3e4bf1149da870731f23
SebastianS-HTL <hexatex.sesh@gmail.com>
2025-07-01 11:32:34 +0200

    move raycasting to its own function

--- project/Program.cs
+++ project/Program.cs
@@ -2,23 +2,24 @@
 {
     internal class Program
     {
+        static int[,] map = new int[11, 11];
+
+        static (double, double) playerPosition = (6, 6);
+
+        static int fov = 60; // in degrees
+        static double viewdistance = 10.0; // in mapunits(one index in the map array is one mapunit)
+        static double playerRotation = 0; // in radians
+        static double turnSpeed = (2 * Math.PI) / 360; // fuck deltatime
+
+        static double raySpeed = 0.2;
+        static double raySpread = 0.1;
+        static double oobValue = 0; // returned if a bay would spawn out of bounds
+
         static void Main(string[] args)
         {
             // id´s of maptiles:
             // 0 = empty
             // 1 = wall
-            int[,] map = new int[11, 11];
-
-            (double, double) playerPosition = (6, 6);
-
-            int fov = 60; // in degrees
-            double viewdistance = 10.0; // in mapunits(one index in the map array is one mapunit)
-            double playerRotation = 0; // in radians
-            double turnSpeed = (2*Math.PI)/360; // fuck deltatime
-
-            double raySpeed = 0.2;
-            double raySpread = 0.1;
-            double oobValue = 0; // returned if a bay would spawn out of bounds
 
             // create test map
             for (int i = 0; i < map.GetLength(0); i++)
@@ -42,9 +43,7 @@
                 }
             }
 
-            // visualize the testmap
-            printMap(map);
-
+            // "game" loop
             while (true)
             {
                 char c = Console.ReadKey(true).KeyChar;
@@ -58,53 +57,7 @@
                     playerRotation = (playerRotation - turnSpeed + Math.PI * 2) % (Math.PI * 2);
                 }
 
-                (double, double) ray = (Math.Sin(playerRotation), Math.Cos(playerRotation));
-
-                (double, double) rayposition = playerPosition;
-                double raydistance = 0;
-
-                //simulate the ray
-                for (double i = 0; i < viewdistance; i += raySpeed)
-                {
-                    if (ray.Item1 > 0)
-                    {
-                        rayposition.Item1 += ray.Item1*raySpeed;
-                    }
-
-                    if (ray.Item1 < 0)
-                    {
-                        rayposition.Item1 -= ray.Item1 * raySpeed;
-                    }
-
-                    if (ray.Item2 > 0)
-                    {
-                        rayposition.Item2 += ray.Item2 * raySpeed;
-                    }
-
-                    if (ray.Item2 < 0)
-                    {
-                        rayposition.Item2 -= ray.Item2 * raySpeed;
-                    }
-
-
-                    try
-                    {
-                        if (map[(int)Math.Round(rayposition.Item1), (int)Math.Round(rayposition.Item2)] != 0)
-                        {
-                            break;
-                        }
-                    }
-                    catch(IndexOutOfRangeException e)
-                    {
-                        raydistance = 0;
-                        break;
-                    }
-
-                    raydistance = i;
-
-                }
-
-                //Console.SetCursorPosition(0, 0);
+                double raydistance = castRay(playerPosition, playerRotation);
 
                 for (int i = 0; i < raydistance * 10; i++)
                 {
@@ -112,6 +65,56 @@
                 }
                 Console.WriteLine();
             }
+        }
+
+        static double castRay((double,double)origin, double direction)
+        {
+            (double, double) ray = (Math.Sin(direction), Math.Cos(direction));
+
+            (double, double) rayposition = origin;
+            double raydistance = oobValue;
+
+            //simulate the ray
+            for (double i = 0; i < viewdistance; i += raySpeed)
+            {
+                if (ray.Item1 > 0)
+                {
+                    rayposition.Item1 += ray.Item1 * raySpeed;
+                }
+
+                if (ray.Item1 < 0)
+                {
+                    rayposition.Item1 -= ray.Item1 * raySpeed;
+                }
+
+                if (ray.Item2 > 0)
+                {
+                    rayposition.Item2 += ray.Item2 * raySpeed;
+                }
+
+                if (ray.Item2 < 0)
+                {
+                    rayposition.Item2 -= ray.Item2 * raySpeed;
+                }
+
+
+                try
+                {
+                    if (map[(int)Math.Round(rayposition.Item1), (int)Math.Round(rayposition.Item2)] != 0)
+                    {
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    break;
+                }
+
+                raydistance = i;
+
+            }
+
+            return raydistance;
         }
 
         static void printMap(int[,] map)
