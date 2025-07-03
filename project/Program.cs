933affe5b65fa9bf2acae254f03837dd68d8ceb2
Sebastian Schneiderbauer <hexatex.sesh@gmail.com>
2025-07-03 21:12:06 +0200

    add map importing via png files

--- project/Program.cs
+++ project/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HK._3d.doomRenderer
+﻿using System.Drawing;
+
+namespace HK._3d.doomRenderer
 {
     internal class Program
     {
@@ -6,7 +8,7 @@
 
         static (double, double) playerPosition = (2, 2);
 
-        static int fov = 111; // in degrees
+        static int fov = 100; // in degrees
         static double rad = Math.PI / 180;
         static double viewdistance = 100.0; // in mapunits(one index in the map array is one mapunit)
         static double playerRotation = 0; // in radians
@@ -19,14 +21,61 @@
 
         static int windowHeight = (fov / 16) * 9; // times 2 multiplier;
 
+        static void ImportMapFromPng(string path)
+        {
+            using (var bmp = new Bitmap(path))
+            {
+                int height = bmp.Height;
+                int width = bmp.Width;
+                map = new int[height, width];
+
+                bool playerSet = false;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        Color pixel = bmp.GetPixel(x, y);
+
+                        if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
+                        {
+                            map[y, x] = 1; // Wall
+                        }
+                        else if (pixel.R == 255 && pixel.G == 255 && pixel.B == 255)
+                        {
+                            map[y, x] = 0; // Empty
+                        }
+                        else if (pixel.R == 255 && pixel.G == 0 && pixel.B == 0)
+                        {
+                            map[y, x] = 0; // Player spawn is empty
+                            if (!playerSet)
+                            {
+                                playerPosition = (y, x);
+                                playerSet = true;
+                            }
+                        }
+                        else
+                        {
+                            // Treat any other color as empty
+                            map[y, x] = 0;
+                        }
+                    }
+                }
+
+                if (!playerSet)
+                    throw new Exception("No player spawn (pure red pixel) found in map image.");
+            }
+        }
+
         static void Main(string[] args)
         {
             // id´s of maptiles:
             // 0 = empty
             // 1 = wall
 
-            // create test map
-            createTestMap(1);
+            // create map
+            //createTestMap(1);
+            ImportMapFromPng("../../../maps/map.png");
 
             // "game" loop
             Console.CursorVisible = false;
@@ -58,6 +107,12 @@
                 else if (c == 'm')
                 {
                     printMap();
+                }
+                else if (c == 'q')
+                {
+                    Console.Clear();
+                    Console.CursorVisible = true;
+                    Environment.Exit(0);
                 }
             }
         }
@@ -138,20 +193,6 @@
             drawWalls(rayDistances);
         }
 
-        // wrong fisheye approach
-        static void renderFisheyeFrame()
-        {
-            List<double> rayDistances = new List<double>();
-
-            for (int i = fov / -2; i <= fov / 2; i++)
-            {
-                double change = i * rad;
-                rayDistances.Add(castRay(playerPosition, playerRotation + change));
-            }
-
-            drawWalls(rayDistances);
-        }
-
         static void drawWalls(List<double> rayDistances)
         {
             Console.SetCursorPosition(0, 0);
@@ -221,7 +262,7 @@
 
             frame += "┘";
 
-            Console.Write(frame+ "\n│ w:   forewards       │\n│ s:   backwards       │\n│ a/d: turn left/right │\n│ m:   toggle map      │\n└──────────────────────┘");
+            Console.Write(frame+ "\n│ W:   forewards       │\n│ S:   backwards       │\n│ A/D: turn left/right │\n│ M:   toggle map      │\n│ Q:   quit            │\n└──────────────────────┘");
         }
 
         static double castRay((double, double) origin, double direction)
@@ -287,6 +328,8 @@
 
                 Console.WriteLine();
             }
+
+            Console.Write("press M to toggle map");
 
             while (true)
             {
